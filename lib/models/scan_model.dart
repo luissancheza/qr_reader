f43b3b2eee4271642879064530f6a81a07b034c8
Luis <luis_sanchez_arenas@hotmail.com>
2021-08-01 22:04:39 -0500

    first commit

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final scanModel = scanModelFromMap(jsonString);
+
+import 'dart:convert';
+
+class ScanModel {
+  ScanModel({
+    this.id,
+    this.tipo,
+    required this.valor,
+  });
+
+  int? id;
+  String? tipo;
+  String valor;
+
+  factory ScanModel.fromJson(String str) => ScanModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory ScanModel.fromMap(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        tipo: json["tipo"],
+        valor: json["valor"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "tipo": tipo,
+        "valor": valor,
+      };
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navigatorBar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+import 'mapas_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Center(child: Text('Historial')),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Obtener el select menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    DBProvider.db.database;
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+}
